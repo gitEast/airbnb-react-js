9b731b1b503f6029d9325b47f0310e2f99f1f13a
EastWind <linxiayoudongfeng@gmail.com>
2023-06-22 09:21:32 +0800

    logo 点击跳转至首页

--- src/components/app-header/c-cpns/header-left/style.js
+++ src/components/app-header/c-cpns/header-left/style.js
@@ -2,5 +2,10 @@ import { styled } from 'styled-components';
 
 export const LeftWrapper = styled.div`
   flex: 1;
+  display: flex;
   color: ${(props) => props.theme.color.primary};
+
+  .logo {
+    cursor: pointer;
+  }
 `;
